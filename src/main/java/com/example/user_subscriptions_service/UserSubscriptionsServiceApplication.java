7527c7d9ddd8207bc1015a6688da64764c3dc539
OcolusX <melchakow.d@mail.ru>
2025-05-06 20:26:12 +0300

    Инициализация проекта + подключение БД

---
+++ src/main/java/com/example/user_subscriptions_service/UserSubscriptionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_subscriptions_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSubscriptionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSubscriptionsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/user_subscriptions_service/UserSubscriptionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_subscriptions_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSubscriptionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
